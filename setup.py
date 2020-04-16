206b00bf36cfdd3635894f99b4177fe4acb26d87
Qkrisi <qkrisiyt@gmail.com>
2020-04-16 11:50:53 +0200

    1.0.1

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from distutils.core import setup
 setup(
   name = 'ReflectionHelper',
   packages = ['ReflectionHelper'],
-  version = '1.0',
+  version = '1.0.1',
   license='MIT',
   description = 'A reflection module based on the C# System.Reflection library',
+  long_description=local_file('README.md'),
+  long_description_content_type='text/markdown',
   author = 'Qkrisi',
   author_email = 'qruczkristof@gmail.com',
   url = 'https://github.com/Qkrisi/ReflectionHelper',
-  download_url = 'https://github.com/Qkrisi/ReflectionHelper/archive/v_1_0.tar.gz',
+  download_url = 'https://github.com/Qkrisi/ReflectionHelper/archive/v_1_0_1.tar.gz',
   keywords = ['Reflection', 'Helper'],
-  install_requires=
+  install_requires=[
 			'forbiddenfruit',
       ],
   classifiers=[
